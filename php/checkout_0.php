a49d4bd2bc3876d5f0ac7ea23e19ec485c93c979
gowithnk <gowithnk@gmail.com>
2022-09-15 13:23:23 +0530

    plugins installed

--- php/checkout_0.php
+++ php/checkout_0.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,55 +8,40 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Le styles -->
-
-
-
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/loader-style.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
-     <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/custom.css">
 
     <link rel="stylesheet" type="text/css" href="assets/js/progress-bar/number-pb.css">
 
-
-
     <style type="text/css">
-    canvas#canvas4 {
-        position: relative;
-        top: 20px;
-    }
+        canvas#canvas4 {
+            position: relative;
+            top: 20px;
+        }
     </style>
-
-
-
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
     <!-- Fav and touch icons -->
     <link rel="shortcut icon" href="assets/ico/minus.png">
 </head>
 
 <body>
     <!-- Navbar -->
-<?php
+    <?php
 
-include('../topbar.php');
+    include('../topbar.php');
 
-?>
+    ?>
 
     <!-- /END OF TOP NAVBAR -->
 
     <!-- SIDE MENU -->
 
-   <?php include('../sidebar.php');
+    <?php include('../sidebar.php');
 
 
-?>
+    ?>
     <!-- END OF SIDE MENU -->
-
-
 
     <!--  PAPER WRAP -->
     <div class="wrap-fluid">
@@ -65,47 +49,43 @@ include('../topbar.php');
             <div class="content-wrap">
                 <div class="row">
                     <div class="col-sm-6" style="display: none ">
-                        <div class="chart-wrap" >
+                        <div class="chart-wrap">
                             <div class="chart-dash">
-                                
+
                                 <div id="placeholder" style="width:100%;height:200px;"></div>
                             </div>
-                        </div>  
+                        </div>
                     </div>
 
-                <!-- center page-->
-                <?php include('checkout.php');?>                         
+                    <!-- center page-->
+                    <?php include('checkout.php'); ?>
+                </div>
+            </div>
+
+            <!-- /END OF CONTENT -->
+
+
+            <!-- FOOTER -->
+            <div class="footer-space"></div>
+            <div id="footer">
+                <div class="devider-footer-left"></div>
+                <div class="time">
+                    <p id="spanDate"></p>
+                    <p id="clock"></p>
+                </div>
+                <div class="copyright">
+                    <span class="entypo-heart"></span>2022 <a href="http://synokempharma.com">Synokem</a> All Rights Reserved
+                </div>
+                <div class="devider-footer"></div>
+            </div>
+            <!-- / END OF FOOTER -->
         </div>
     </div>
-             
-                <!-- /END OF CONTENT -->
-
-
-
-                <!-- FOOTER -->
-                <div class="footer-space"></div>
-                <div id="footer">
-                    <div class="devider-footer-left"></div>
-                    <div class="time">
-                        <p id="spanDate"></p>
-                        <p id="clock"></p>
-                    </div>
-                    <div class="copyright">
-                        <span class="entypo-heart"></span>2022 <a href="http://synokempharma.com">Synokem</a> All Rights Reserved</div>
-                    <div class="devider-footer"></div>
-
-
-                </div>
-                <!-- / END OF FOOTER -->
-
-
-            </div>
-        </div>
     </div>
     <!--  END OF PAPER WRAP -->
 
     <!-- RIGHT SLIDER CONTENT -->
-    
+
     <!-- END OF RIGHT SLIDER CONTENT-->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.js"></script>
     <script src="assets/js/progress-bar/src/jquery.velocity.min.js"></script>
@@ -127,68 +107,67 @@ include('../topbar.php');
     <!-- GAGE -->
 
 
-    
+
     <script src="assets/js/jhere-custom.js"></script>
 
     <script>
-    var gauge4 = new Gauge("canvas4", {
-        'mode': 'needle',
-        'range': {
-            'min': 0,
-            'max': 90
-        }
-    });
-    gauge4.draw(Math.floor(Math.random() * 90));
-    var run = setInterval(function() {
+        var gauge4 = new Gauge("canvas4", {
+            'mode': 'needle',
+            'range': {
+                'min': 0,
+                'max': 90
+            }
+        });
         gauge4.draw(Math.floor(Math.random() * 90));
-    }, 3500);
+        var run = setInterval(function() {
+            gauge4.draw(Math.floor(Math.random() * 90));
+        }, 3500);
     </script>
 
 
     <script type="text/javascript">
-    /* Javascript
-     *
-     * See http://jhere.net/docs.html for full documentation
-     */
-   
+        /* Javascript
+         *
+         * See http://jhere.net/docs.html for full documentation
+         */
     </script>
     <script type="text/javascript">
-    var output, started, duration, desired;
+        var output, started, duration, desired;
 
-    // Constants
-    duration = 5000;
-    desired = '50';
+        // Constants
+        duration = 5000;
+        desired = '50';
 
-    // Initial setup
-    output = $('#speed');
-    started = new Date().getTime();
+        // Initial setup
+        output = $('#speed');
+        started = new Date().getTime();
 
-    // Animate!
-    animationTimer = setInterval(function() {
-        // If the value is what we want, stop animating
-        // or if the duration has been exceeded, stop animating
-        if (output.text().trim() === desired || new Date().getTime() - started > duration) {
-            console.log('animating');
-            // Generate a random string to use for the next animation step
-            output.text('' + Math.floor(Math.random() * 60)
+        // Animate!
+        animationTimer = setInterval(function() {
+            // If the value is what we want, stop animating
+            // or if the duration has been exceeded, stop animating
+            if (output.text().trim() === desired || new Date().getTime() - started > duration) {
+                console.log('animating');
+                // Generate a random string to use for the next animation step
+                output.text('' + Math.floor(Math.random() * 60)
 
-            );
+                );
 
-        } else {
-            console.log('animating');
-            // Generate a random string to use for the next animation step
-            output.text('' + Math.floor(Math.random() * 120)
+            } else {
+                console.log('animating');
+                // Generate a random string to use for the next animation step
+                output.text('' + Math.floor(Math.random() * 120)
 
-            );
-        }
-    }, 5000);
+                );
+            }
+        }, 5000);
     </script>
     <script type="text/javascript">
-    $('#getting-started').countdown('2015/01/01', function(event) {
-        $(this).html(event.strftime(
+        $('#getting-started').countdown('2015/01/01', function(event) {
+            $(this).html(event.strftime(
 
-            '<span>%M</span>' + '<span class="start-min">:</span>' + '<span class="start-min">%S</span>'));
-    });
+                '<span>%M</span>' + '<span class="start-min">:</span>' + '<span class="start-min">%S</span>'));
+        });
     </script>
 
 
